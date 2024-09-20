20da16c264efb2ac7c5923c3039a78f1a32ab70e
MkTz <mlcnierras@gmail.com>
2024-09-20 12:48:23 +0800

    fix: function animate

--- main.js
+++ main.js
@@ -3,8 +3,27 @@ import WebGL from 'three/addons/capabilities/WebGL.js';
 
 if ( WebGL.isWebGL2Available() ) {
 
-	// Initiate function or other initializations here
-	animate();
+	const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ffff  } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    function animate() {
+        renderer.render( scene, camera );
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    }
+    renderer.setAnimationLoop( animate );
 
 } else {
 
@@ -12,25 +31,3 @@ if ( WebGL.isWebGL2Available() ) {
 	document.getElementById( 'container' ).appendChild( warning );
 
 }
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff  } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	renderer.render( scene, camera );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-}
-renderer.setAnimationLoop( animate );
